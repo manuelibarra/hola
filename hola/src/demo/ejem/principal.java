ed19072a59308fa28e0e1ca5249fc7153aa1500b
manuel <manuelibarra@gmail.com>
2012-12-30 07:47:38 -0500

    segundo commit

--- hola/src/demo/ejem/principal.java
+++ hola/src/demo/ejem/principal.java
@@ -9,7 +9,8 @@ public class principal {
 	
 	public static void main (String args [])
 	{
-		System.out.println("hola");
+		System.out.println("hola mundo cruel");
+		System.out.println("segundo cambio");
 	}
 
 }
